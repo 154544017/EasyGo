1e8dc726a6d48b4433f3b08122065ed40c5d393f
weixiaonan <154544017@qq.com>
2019-01-02 12:32:37 +0800

    修复成就游记不同步的问题

--- app/src/main/java/com/software/tongji/easygo/MapMvp/MapFragment.java
+++ app/src/main/java/com/software/tongji/easygo/MapMvp/MapFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -103,10 +104,8 @@ public class MapFragment extends Fragment implements MapContract.MapView {
     }
 
     @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
-        if(!hidden){
-            updateAchievements();
-        }
+    public void onResume() {
+        super.onResume();
+        updateAchievements();
     }
 }
